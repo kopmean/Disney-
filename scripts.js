4d62c551bed8a66fd2df45c7550b69385561b004
larkingz01 <63070017@kmitl.ac.th>
2021-11-04 14:11:41 +0700

    Update Slide by using js

---
+++ scripts.js
@@ -0,0 +1,17 @@
+let requestURL = 'action.json';
+let request = new XMLHttpRequest();
+request.onreadystatechange = function () {
+    if (request.readyState == 4 && request.status == 200) {
+        dataReportStatus(JSON.parse(request.responseText));
+    }
+};
+request.open("GET", requestURL, true);
+request.send();
+function dataReportStatus(data){
+    document.getElementById("picture").innerHTML +="<div class='carousel-item active'><img class='d-block h-50 w-100' src='https://s.isanook.com/ga/0/rp/r/w728/ya0xa0m1w0/aHR0cHM6Ly9zLmlzYW5vb2suY29tL2dhLzAvdWQvMjEyLzEwNjAwMTEvbG9sLTcwMHg0MDAuanBn.jpg' alt='Third slide'></div>";
+    // for(let i=0; i<data.length;i++){
+    //     document.getElementById("rows3").innerHTML +="<div class='col-sm-2 card' id='card"+i+"'style='width: 18rem;'><img class='card-img-top' src='"+data[i].image+"' alt='Card image cap'></div>";
+    //     document.getElementById("card"+i).innerHTML += "<div class='card-body'><h6 class='card-title font-weight-bold'  style='font-size:16px;'>"+data[i].song+"</h6>"
+    //     document.getElementById("card"+i).innerHTML += "<p class='card-text' style='font-size:13px;'>"+data[i].artist+"</p>"+"</div>";
+    // }
+}
