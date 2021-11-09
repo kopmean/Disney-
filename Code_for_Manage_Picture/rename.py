f7e3e1f5a12c69e6a255cb6a7fb072d621e65bc9
Kamolchanok Sinraksa <77113968+zolocapricorn@users.noreply.github.com>
2021-11-09 20:50:26 +0700

    Refresh

--- Code_for_Manage_Picture/rename.py
+++ Code_for_Manage_Picture/rename.py
@@ -1,7 +1,7 @@
 import os
 
 def main():
-    paths = "E:/Github/Disney-/Disney_Plus_Hotstar/image/"
+    paths = "E:/Github/Disney-/Disney_Plus_Hotstar/image/New_to_Disney+_Hotstar/"
     for count, filename in enumerate(os.listdir(paths)):
         dst = str(count+1) + ".jpg"
         src = paths+filename
