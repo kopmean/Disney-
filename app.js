b6495272606e8fec9da932407668763998874d84
larkingz01 <63070017@kmitl.ac.th>
2021-11-12 21:45:34 +0700

    Update Slide

---
+++ app.js
@@ -0,0 +1,45 @@
+const carousal = document.querySelector(".carousal");
+let sliders = [];
+let slideIndex = 0;
+
+const createSlide = () => {
+    if(slideIndex >= movies.length){
+        slideIndex = 0;
+    }
+
+    let slide = document.createElement('div');
+    let imgElement = document.createElement('img');
+    let content = document.createElement('div');
+    let h1 = document.createElement('h1');
+    let p = document.createElement('p');
+
+    imgElement.appendChild(document.createTextNode(''));
+    h1.appendChild(document.createTextNode(movies[slideIndex].name));
+    p.appendChild(document.createTextNode(movies[slideIndex].des));
+    content.appendChild(h1);
+    content.appendChild(p);
+    slide.appendChild(content);
+    slide.appendChild(imgElement);
+    carousal.appendChild(slide);
+
+    imgElement.src = movies[slideIndex].image;
+    slideIndex++;
+
+    slide.className = "slider";
+    content.className = "slide-content";
+    h1.className = "movie-title";
+    p.className = "movie-des";
+
+    sliders.push(slide);
+
+    if(sliders.length){
+        sliders[0].style.marginLeft = `calc(-${100 * (sliders.length - 2)}% - ${30 * (sliders.length - 2)}px)`;
+    }
+}
+for(let i=0; i<3; i++){
+    createSlide();
+}
+
+setInterval(() =>{
+    createSlide();
+}, 3000)
